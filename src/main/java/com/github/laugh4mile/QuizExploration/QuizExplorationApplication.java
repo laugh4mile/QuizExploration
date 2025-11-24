42115fa903c2348ed472dbd462d35cdaa0b58347
Hyunsoo <laugh4mile@naver.com>
2025-11-24 15:09:48 +0900

    Initial commit

---
+++ src/main/java/com/github/laugh4mile/QuizExploration/QuizExplorationApplication.java
@@ -0,0 +1,13 @@
+package com.github.laugh4mile.QuizExploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizExplorationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizExplorationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/laugh4mile/QuizExploration/QuizExplorationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.laugh4mile.QuizExploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizExplorationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
