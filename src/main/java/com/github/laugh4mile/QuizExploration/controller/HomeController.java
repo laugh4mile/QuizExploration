4f79c07273c63e4c692619637d9ed4c8dcdf40ee
Hyunsoo <laugh4mile@naver.com>
2025-11-25 11:02:51 +0900

    HomeController 생성 및 index 파일 생성

---
+++ src/main/java/com/github/laugh4mile/QuizExploration/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.github.laugh4mile.QuizExploration.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // resources/templates/index.html을 반환
+    }
+
+}
