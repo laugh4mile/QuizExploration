586d0e2e8ff9feb144105cd10b4d304c7dc6974f
Hyunsoo <laugh4mile@naver.com>
2025-11-26 16:34:42 +0900

    Service 기능 추가

--- src/main/java/com/github/laugh4mile/quizexploration/service/ExamService.java
+++ src/main/java/com/github/laugh4mile/quizexploration/service/ExamService.java
@@ -1,12 +1,23 @@
 package com.github.laugh4mile.quizexploration.service;
 
+import com.github.laugh4mile.quizexploration.entity.Exam;
 import com.github.laugh4mile.quizexploration.repository.ExamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ExamService {
 
     private final ExamRepository examRepository;
+
+    public List<Exam> findAllExams() {
+        return examRepository.findAll();
+    }
+
+    public Exam saveExam(Exam exam) {
+        return examRepository.save(exam);
+    }
 }
